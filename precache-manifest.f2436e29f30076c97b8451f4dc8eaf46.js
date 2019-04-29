fdeaf11bd827fe207c2552c3379c354c4912d92f
robertjdominguez <rtjdominguez@gmail.com>
2019-04-29 10:19:11 -0500

    Updates

---
+++ precache-manifest.f2436e29f30076c97b8451f4dc8eaf46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0700f2abdf768a6f8ee4ff96fd694d",
+    "url": "/headsUp/index.html"
+  },
+  {
+    "revision": "c448343712c460ef5464",
+    "url": "/headsUp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ac3f8bc14bde6453d5af",
+    "url": "/headsUp/static/css/main.40f0e25f.chunk.css"
+  },
+  {
+    "revision": "c448343712c460ef5464",
+    "url": "/headsUp/static/js/2.c1c88dd1.chunk.js"
+  },
+  {
+    "revision": "ac3f8bc14bde6453d5af",
+    "url": "/headsUp/static/js/main.edcba1e0.chunk.js"
+  },
+  {
+    "revision": "0bfef3f271e55c6c294e",
+    "url": "/headsUp/static/js/runtime~main.600ed30b.js"
+  }
+]);
